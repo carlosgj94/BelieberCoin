6e8ca7c494458e15a30cc8b2984b674c74fa622a
Carlos Gonzalez Juarez <carlosgj94@localhost.localdomain>
2017-06-01 21:06:14 +0200

    Added more data to the constructor and some checks before any transfer

--- BelieberCoin.sol
+++ BelieberCoin.sol
@@ -3,12 +3,24 @@ pragma solidity 0.4.11;
 contract BelieberCoin {
     mapping (address => uint256) public balanceOf;
     //balanceOf[address] = 5;
+    string public standart = "BelieberCoin v1.0";
+    string public name;
+    string public symbol;
+    uint8 public decimal;
+    uint256 public totalSupply;
 
-    function BelieberCoin(uint256 initialSupply) {
+    function BelieberCoin(uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits) {
       balanceOf[msg.sender] = initialSupply;
+      totalSupply = initialSupply;
+      decimal = decimalUnits;
+      symbol = tokenSymbol;
+      name = tokenName;
     }
 
     function transfer(address _to, uint256 _value) {
+      if(balanceOf[msg.sender] < _value) throw;
+      if(balanceOf[_to] + _value < balanceOf[_to]) throw;
+
       balanceOf[msg.sender] -= _value;
       balanceOf[_to] += _value;
     }
