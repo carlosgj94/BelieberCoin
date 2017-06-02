288a3f0030f77aa3ae827339426cb0f3464ef8ce
Carlos Gonzalez Juarez <carlosgj94@gmail.com>
2017-06-02 11:27:47 +0200

    Added the ability to create/mint new money. Only for the admin

--- BelieberCoin.sol
+++ BelieberCoin.sol
@@ -77,4 +77,11 @@ contract BelieberCoinAdvanced is admined, BelieberCoin {
     balanceOf[admin] = initialSupply;
     totalSupply = initialSupply;
   }
+
+  function mintToken( address target, uint256 mintedAmount) onlyAdmin {
+    balanceOf[target] += mintedAmount;
+    totalSupply += mintedAmount;
+    Transfer(0, this, mintedAmount);
+    Transfer(this, target, mintedAmount);
+  }
 }
